984054bfd9d69b6b71e559100f1c0b268922e8f2
yjs000 <jisu91113@gmail.com>
2022-04-13 18:02:32 +0900

    set project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <BootstrapTest />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import BootstrapTest from "./components/BootstrapTest.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    BootstrapTest
   }
 }
 </script>
